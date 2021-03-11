fd0fcdeacd07a3e08683c239f61fa3d313424ec8
peterliao <peter@www.ucs.com>
2021-03-11 11:37:24 +0800

    Initial Commit

---
+++ HeIsJustAKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeIsJustAKid
+//
+//  Created by peter on 2021/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
